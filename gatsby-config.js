9a64b3bc72a5381aa0c9ff4d4a4077b2e843b049
simjohansson <simjohansson@gmail.com>
2019-10-01 20:21:35 +0200

    welcome text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Caroline & Simon`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: ``,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -13,6 +13,14 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Tangerine']
+        }
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,64 @@ const  useStyles = makeStyles(theme => ({
     display: 'block',
     marginLeft: 'auto',
     marginRight: 'auto'
-  }
+  },
+  imageContainer: {
+    position: 'relative'
+  },
+  titleTextBlock: {
+    position: 'absolute',
+    top: '5rem',
+    right: '20%',
+   // backgroundColor: 'rgba(0,0,0,.7)',
+    color: 'white',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    fontFamily: 'Tangerine',
+    fontSize: '7vw',
+    "@media screen and (max-width: 600px)":{
+      display: 'none'
+    }
+  },
+  infoTextBlock: {
+    position: 'absolute',
+    top: '10rem',
+    right: '0rem',
+   // backgroundColor: 'rgba(0,0,0,.7)',
+    color: 'white',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    fontFamily: 'Tangerine',
+    fontSize: '5vw',
+    maxWidth: '40vw',
+    lineHeight: '1em',
+    "@media screen and (max-width: 600px)":{
+      display: 'none'
+    }
+  },
+  mobileText: {
+    display: 'none',
+    fontFamily: 'Tangerine',
+    fontSize: '3rem',
+    "@media screen and (max-width: 600px)":{
+      display: 'block'
+    }
+
+  },
+    title: { fontSize: '5rem', marginTop: '2rem', marginBottom: '2rem'}
 }))
 
 const IndexPage = () =>{
   const classes = useStyles();
-  
+  const title = 'Hej!';
+  const infoText = 'Kul att du har hittat hit! Här kommer information komma om bröllopet så håll utkik!';
   return (
   <Layout>
     <SEO title="Föra bröllop" />
-    <HeadImage/> 
+    <div className={classes.imageContainer}>
+    <HeadImage/>
+    <div className={classes.titleTextBlock}>{title}</div>
+    <div className={classes.infoTextBlock}>{infoText}</div> </div>
+    <div className={classes.mobileText}><div className={classes.title}>{title}</div><p>{infoText}</p></div>
     <img className={classes.centerImage} src={underconstructiongif} />
   </Layout>
 );
