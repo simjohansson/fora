9f76b396602494aed45575180b287ed753d0997b
simjohansson <simjohansson@gmail.com>
2019-11-27 22:25:58 +0100

    Improvements in accomendation

--- src/pages/accomendation.js
+++ src/pages/accomendation.js
@@ -15,30 +15,30 @@ const accomendations = [
   other: "Ganska litet ställe",
   website: "https://olandsro.se/",
   booking: "Kontakta bröllopsparet",
-  breakfast: "Ingår med boendet"
+  breakfast: "Ingår"
 },
 {
   title: "Stf Hagaby",
-  description: "Stort vandrahem! Här kommer en hel hög av brudenssläkt befinna sig.",
+  description: "Stort vandrahem! Här kommer en hel hög av brudens släkt befinna sig.",
   distanceToParty: "19 km",
   typeOfRooms: "Stuga, hängmatta, två till fyrbäddsrum.",
   priceCategory: "Billigare",
   other: "Här kommer det antagligen arrangeras en grillning på fredagen innan bröllopet. Bastu och utegym finns tillgängligt.",
   website: "https://www.svenskaturistforeningen.se/boende/stf-hagabylantgarden-vandrarhem/",
-  booking: "Kontakta brudens mormor",
-  breakfast: "Måste köpas till"
+  booking: "Kontakta ansvariga.",
+  breakfast: "Köp till eller fixa eget."
 
 },
 {
   title: "Äleklintagård",
-  description: "Önskas det ett morgondopp? Här bor man i gränslandet mellan alvaret och strandkanten.",
+  description: "Önskas det ett morgondopp? Här bor man i gränslandet mellan Alvaret och strandkanten.",
   distanceToParty: "12 km",
-  typeOfRooms: "Rum i ett hus, 2 fyrbäddsrum och 1 tvåbäddsrum",
+  typeOfRooms: "Rum i ett hus, 2 fyrbäddsrum och 1 tvåbäddsrum.",
   priceCategory: "-",
   other: "",
   website: "http://aleklintagard.se/",
-  booking: "Nämn Simon vid bokning, då han har preliminärt bokat rummen. Hör gärna av dig till brudparet om du har bokat!",
-  breakfast: "Tillgång till kök"
+  booking: "Nämn Simon vid bokning, då rummen är preliminärt bokade. Hör gärna av dig till brudparet om du har bokat!",
+  breakfast: "Tillgång till kök."
 
 },
 {
@@ -47,27 +47,32 @@ const accomendations = [
   distanceToParty: "15 km",
   typeOfRooms: "Två hus med flera dubbelrum",
   priceCategory: "-",
-  other: "Hela huset måste hyras, perfekt för er som vill gå ihop.",
+  other: "Hela huset måste hyras, perfekt för er som vill bo ihop.",
   website: "http://wannborga.se/",
-  booking: "Kontakta brudparet",
+  booking: "Kontakta brudparet.",
   breakfast: "Oklart"
 
 },
 {
 title: "Festlokalen",
-description: "Tänker du gå sist från festen? Varför inte bo på plats!",
+description: "Tänker du gå sist från festen? Bo gratis på plats!",
 distanceToParty: "En trappa",
 typeOfRooms: "Loft och bibliotek",
 priceCategory: "Gratis",
 other: "Ta med sovsäck och liggunderlag/madrass. Obs! Dusch saknas.",
 website: "",
 booking: "Kontakta brudparet",
-breakfast: "Saknas, (Kvarglömda chips)"
+breakfast: "Saknas om du inte har tur att hitta några kvarglömda chips."
 
 },
 ]
 
 
+const otherPlaces = {
+  title: "Andra ställen att kolla in...",
+  places: ["Källa hamn", "Wikegårds camping", "Södviks gästgivaregård", "Alvaret hotel och hostel", "Highway 136"]
+  
+  }
 const  useStyles = makeStyles(theme => ({
   accomendationPaper: {
     marginTop: "3rem",
@@ -98,7 +103,7 @@ const AccomendationPage = (props) => {
        <Paper key={index} className={classes.accomendationPaper}>
         <Typography variant="h3">{accomendation.title}</Typography>
         <Typography className={classes.accomendationDescription} variant="body1">{accomendation.description}</Typography>
-        <Table className={classes.table} aria-label="simple table">
+        {accomendation.distanceToParty &&  <Table className={classes.table} aria-label="simple table">
           <TableBody>
         <TableRow><TableCell><Typography className={classes.noWrapLabel} variant="body1">Avstånd till festlokal: </Typography></TableCell>
        <TableCell><Typography  variant="body1">{accomendation.distanceToParty}</Typography></TableCell></TableRow>    
@@ -106,18 +111,26 @@ const AccomendationPage = (props) => {
        <TableCell><Typography  variant="body1">{accomendation.typeOfRooms}</Typography></TableCell></TableRow>
        <TableRow><TableCell><Typography variant="body1">Frukost: </Typography></TableCell>
        <TableCell><Typography  variant="body1">{accomendation.breakfast}</Typography></TableCell></TableRow>
-       <TableRow><TableCell><Typography variant="body1">Prisnivå: </Typography></TableCell>
-       <TableCell><Typography  variant="body1">{accomendation.priceCategory}</Typography></TableCell></TableRow>
        <TableRow><TableCell><Typography variant="body1">Hemsida: </Typography></TableCell>
        <TableCell><Link variant="body1" href={accomendation.website} target="_blank">Klicka här</Link></TableCell></TableRow>   
        <TableRow><TableCell><Typography variant="body1">Vid bokning: </Typography></TableCell>
        <TableCell><Typography  variant="body1">{accomendation.booking}</Typography></TableCell></TableRow>  
        <TableRow><TableCell><Typography variant="body1">Annat: </Typography></TableCell>
        <TableCell><Typography  variant="body1">{accomendation.other}</Typography></TableCell></TableRow> 
-       </TableBody> </Table>
+       </TableBody> </Table>}
        
        </Paper>)}
-       
+       <Paper  className={classes.accomendationPaper}>
+        <Typography variant="h3">{otherPlaces.title}</Typography>
+        <ul>
+          {otherPlaces.places.map((x, index) => 
+          <li key={index}>
+            <Typography variant="body1">
+            {x}
+            </Typography>
+          </li>)}
+        </ul>
+        </Paper>
       </Container>
     </Layout>
   );
