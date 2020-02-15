f8f0780e6b352686efe216e8c6c0234a5cf8b388
simjohansson <simjohansson@gmail.com>
2020-02-15 22:03:19 +0100

    fix rsvp h1 to h2

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -9,7 +9,7 @@ const RSVPPage = () => {
       <SEO title="RSVP" />
       <FlowersImage/>
       <Container>
-          <Typography variant="h1"><FormattedMessage  id="rsvp.title"/></Typography>
+          <Typography variant="h2"><FormattedMessage  id="rsvp.title"/></Typography>
           <Typography variant="body1"><FormattedHTMLMessage  id="rsvp.firstSection"/></Typography>
           <Typography variant="body1"><FormattedMessage  id="rsvp.bbqInfo"/></Typography>
           <Typography variant="body1"><a style={{color: "darkgreen"}} target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfzuK7ksDmVoZ7_0jGCi7EUGzx-F93drMlwjHBEZv6AF0AYMA/viewform?usp=sf_link"><FormattedMessage  id="rsvp.rsvpHere"/></a></Typography>
